e6e6d0ca8a6fa5e2a1fd7b842d2db9772fe82994
xqzhang <xqzhang@thoughtworks.com>
2012-10-09 13:41:34 +0800

    will count down and log waiting message

--- app.js
+++ app.js
@@ -29,6 +29,8 @@ app.configure('production', function(){
 // Routes
 
 app.get('/dpm-payment', routes.index);
+app.post('/dpm-payment', routes.dpmPayment);
+
 
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,19 @@
  */
 
 exports.index = function(req, res){
-  setTimeout(function(){
-    res.render('index', { title: 'Express' })
-  }, 30000);
+  res.render('index', { title: 'Express' });
+}
+
+exports.dpmPayment = function(req, res){
+  console.log('dpm payment waiting...');
+  var seconds = 20;
+  var interval = 2;
+  var waiting = setInterval(function(){
+     seconds = seconds - interval;
+     console.log(seconds + " seconds left");
+     if(seconds <= 0){
+       clearInterval(waiting);
+       res.redirect('http://localhost:8088/myaccount/paybill-receipt-cc?token=token&p=MTIzNDU2Nzg5MCw0NTYsMTAw');
+     }
+  }, interval * 1000);
 };
